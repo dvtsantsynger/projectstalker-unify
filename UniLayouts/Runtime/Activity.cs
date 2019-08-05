7b6dfb3dda2a81d1cce3f9a13ebd507fa58a122b
DeusExMachine\root <dvtsants@alarstudios.com>
2019-08-05 16:59:16 +0200

    !

--- UniLayouts/Runtime/Activity.cs
+++ UniLayouts/Runtime/Activity.cs
@@ -3,7 +3,7 @@ using UniLayouts.Views;
 using UnityEngine;
 
 namespace UniLayouts.MVP { 
-    public  class Activity : MonoBehaviour {
+    public  abstract class Activity : MonoBehaviour {
         public virtual TMP_FontAsset[] Fonts    { get; }
         public virtual UIViewGroup     RootView { get; }
         public virtual GameObject      UIObject { get; }
